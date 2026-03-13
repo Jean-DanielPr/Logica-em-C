7a1f944f3b31df22788277e6e4d99a686fde7a8f
JeanDaniel-Pr <jeandaniel78b@gmail.com>
2026-03-12 22:48:41 -0300

    exercicios sobre for, if e break

--- 5 Exercicios For/exe4.c
+++ 5 Exercicios For/exe4.c
@@ -3,18 +3,18 @@ int main(){
     int senha_correta = 1234, tentativa = 0;
 
     printf("LOGIN:\n");
-    for(int cont=1; cont<=3; cont++){
+    for (int cont=1; cont<=3; cont++) {
         printf("-------------------\n");
         printf(" Digite sua senha:\n");
         scanf("%i", &tentativa);
         printf("-------------------\n");
-        if(tentativa == senha_correta){
+        if (tentativa == senha_correta){
             printf("ACESSO LIBERADO!\n");
             break;
-        }else{
+        } else {
             printf("ACESSO NEGADO\n");
         }
-        if(cont == 3){
+        if (cont == 3) {
             printf("-------------------\n");
             printf("Limite de tentativas excedido.\n");
             printf("ACESSO BLOQUEADO\n");
